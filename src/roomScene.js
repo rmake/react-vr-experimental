ce1d7d9a269300f24ff679a13c2d6901a48ad3d7
dycoon <kuroda.dycoon@gmail.com>
2017-04-27 15:24:47 +0900

    add: move button

--- src/roomScene.js
+++ src/roomScene.js
@@ -10,135 +10,126 @@ import {
     Box,
     Scene,
     VrHeadModel,
+    Animated,
 } from 'react-vr';
 import SceneBase from "./sceneBase";
 
 export default class RoomScene extends SceneBase {
 
+    positions = [
+        [-3, 0.5, 1],
+        [-4, 0.5, 1],
+        [-4, 0.5, 0],
+        [-2, 0.5, 2],
+    ]
+
+    roomModel = null;
+
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            roomPosition: [
+                new Animated.Value(-3),
+                new Animated.Value(0.5),
+                new Animated.Value(1)
+            ]
+        };
     }
 
-    componentDidMount() {
-        if (!this.state.basePosition) {
-            var basePosition = VrHeadModel.positionOfHeadMatrix(
-                VrHeadModel.getHeadMatrix());
-            basePosition[0] += 3;
-            this.setState({
-                basePosition: basePosition,
-            });
+    moveTo = (position, e) => {
+        console.log("position " + position);
+        var animations = [];
+        position.forEach((v, i) => {
+            animations.push(
+                Animated.spring(
+                    this.state.roomPosition[i],
+                    {
+                        toValue: position[i],
+                        friction: 10,
+                    }
+                )
+            );
+        });
 
-        }
-    }
+        Animated.parallel(
+            animations
+        ).start();
+    };
+
 
     render() {
 
+        var buttons = [];
+        this.positions.forEach((position, i) => {
+            buttons.push(
+                <VrButton
+                    style={{
+                        flex: 1,
+                        backgroundColor: "#1122aa",
+                        margin: 0.01
+                    }}
+                    key={`buttons${i}`}
+                    onClick={this.moveTo.bind(this, position)}
+                    >
+                    <Text>
+                        {`position${i}`}
+                    </Text>
+                </VrButton>
+            );
+        });
+
         return (
             <View>
-
-                <View style={{
-                    position: 'absolute',
-                    backgroundColor: "#1122aa",
-                    layoutOrigin: [
-                        0.5, 0.5
-                    ],
-                    height: 1,
-                    width: 1,
-                    transform: [
-                        {
-                            translate: this.state.basePosition || [-3, 0.5, 1]
-                        },
-                        {
-                            rotateY: -90,
-                        },
-                    ],
-                }}>
-                    <VrButton
-                        style={{
-                            backgroundColor: "#1122aa",
-                            flex: 1,
-                            margin: 0.01
-                        }}
-                        onClick={() => {
-                            this.replaceScene("menu");
-                        }}
-                        >
-                        <Text>Back To Menu</Text>
-
-                    </VrButton>
+                <View
+                    style={{
+                        position: 'absolute',
+                        layoutOrigin: [
+                            0.5, 0.5
+                        ],
+                        transform: [
+                            {
+                                translate: [-0.54, -1, -1]
+                            },
+                            {
+                                rotateX: -90,
+                            },
+                        ],
+                        flexDirection: 'column',
+                        width: 0.5,
+                        height: 0.5,
+                    }}>
+                    {buttons}
                 </View>
-
-
-
-                <VrButton
+                <Animated.View
                     style={{
                         position: 'absolute',
-                    }}
-                    onClick={() => {
-                        console.log("click box!");
-                    }}
-                    >
-                    <Box
-                        style={{
-                            layoutOrigin: [
-                                0.5, 0.5, 0.5
-                            ],
-                            transform: [
-                                {
-                                    translate: [0, 0, -3]
-                                },
-                            ],
-                            color: "#000000",
-                        }}
-                        dimWidth={1}
-                        dimDepth={1}
-                        dimHeight={1}
-                        />
-                </VrButton>
-
-                <VrButton
-                    style={{
-                        position: 'absolute',
-                    }}
-                    ref={(view) => {this.vrButton = view;}}
-                    onClick={(e, e2, e3) => {
-                        console.log("onClick model!");
-                    }}
-                    onEnter={() => {
-                        console.log("onEnter model!");
-                    }}
-                    onExit={() => {
-                        console.log("onExit model!");
-                    }}
-                    onMove={() => {
-                        console.log("onMove model!");
+                        transform: [
+                            {
+                                rotateY: 90,
+                            },
+                            {
+                                translateX: this.state.roomPosition[0]
+                            },
+                            {
+                                translateY: this.state.roomPosition[1]
+                            },
+                            {
+                                translateZ: this.state.roomPosition[2]
+                            },
+                        ],
                     }}
                     >
                     <Model
-                        style={{
-                            position: 'absolute',
-                            transform: [
-                                {
-                                    rotateY: 90,
-                                },
-                                {
-                                    translate: [-3, 0.5, 1]
-                                },
-                            ]
-                        }}
                         source={{
                             obj:asset("room/room.obj"),
                             mtl:asset("room/room.mtl"),
-                            //obj:asset("cc/creature.obj"),
-                            //mtl:asset("cc/creature.mtl"),
                         }}
                         lit={false}
                         cursorVisibilitySlop={{top: 0.10, bottom: 0.10, left: 0, right: 0}}
                         >
 
                     </Model>
-                </VrButton>
+                </Animated.View>
 
             </View>
         );
